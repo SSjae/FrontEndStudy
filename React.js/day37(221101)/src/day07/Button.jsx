bbb92119fc10b705fbd26a6d1207f3c50b74c426
SSjae <ssjj04022@naver.com>
2022-11-02 19:08:22 +0900

    [React] React 컴포넌트 디자인, css, scss, styled-components

---
+++ React.js/day37(221101)/src/day07/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from 'styled-components'
+
+// 요즘에는 잘 안쓰고 작은 단위로 하는 것으로 주로 사용
+const StyledButton = styled.button`
+// 스타일을 작성
+    border-radius: 5px;
+    border: 1px solid lightgray;
+    padding: 6px 12px;
+    background-color: white;
+`
+
+export default function Button(props) {
+    return <StyledButton>버튼입니다.</StyledButton>
+}
