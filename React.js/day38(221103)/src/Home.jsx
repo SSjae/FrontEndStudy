3dafd4beb03894d6c5460725111932d2bc058e8e
SSjae <ssjj04022@naver.com>
2022-11-03 21:40:41 +0900

    [React] tailwindcss, index.js, 로그인 화면, 계정등록 화면

---
+++ React.js/day38(221103)/src/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home(props) {
+    return (
+        <div>Home 화면 입니다.</div>
+    )
+}
---
+++ React.js/day38(221103)/src/Identify.jsx
@@ -0,0 +1,5 @@
+export default function Identify(props) {
+    return (
+        <div>Identify 화면 입니다.</div>
+    )
+}
