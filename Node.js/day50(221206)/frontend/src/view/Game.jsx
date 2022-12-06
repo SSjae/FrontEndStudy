ab6a4ce2abe68bbb3488af2af80c7bb6258916ac
SSjae <ssjj04022@naver.com>
2022-12-06 22:24:44 +0900

    [Node] React Facebook 프로젝트 4

---
+++ Node.js/day50(221206)/frontend/src/view/Game.jsx
@@ -0,0 +1,8 @@
+import Header from "./Header.jsx"
+
+export default function Home(props) {
+    return <>
+        <Header name="game"/>
+        Game화면
+    </>
+}
---
+++ Node.js/day50(221206)/frontend/src/view/Login.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import IMG_LOGO from "../images/facebook-logo.svg"
+// 반복되는 것을 외부에 컴포넌트를 만들어서 import를 해서 사용
+import {Input, Image, Button, Linebar, Title} from "./Component.jsx"
+import axios from "axios"
+
+export default function Login(props) {
+    const [userid, setUserid] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChangeUserid = (e) => {
+        // console.log(e.target.value);
+        setUserid(e.target.value);
+    }
+    const onChangePassword = (e) => {
+        // console.log(e.target.value);
+        setPassword(e.target.value);
+    }
+
+    const onClickLogin = () => {
+        console.log(userid, password);
+
+        if(!userid) {
+            alert("사용자 계정(이메일)은 반드시 입력해야 합니다.");
+            return
+        } 
+        if(!password) {
+            alert("사용자 비밀번호는 반드시 입력해야 합니다.")
+            return
+        }
+
+        axios.post("/api/login", {userid: userid, password:password}).then((res) => {
+            console.log(res.data);
+            const {result} = res.data
+            if(result === "success") {
+                // alert("로그인 성공");
+                window.location.href = "/home";
+            } else {
+                alert("로그인 실패");
+            }
+        })
+    }
+    return (
+        <div className="login-layer">
+            <div className="logo-box">
+                <Image src={IMG_LOGO} alt="" />
+                <Title text="Facebook에서 전세계에 있는 친구, 가족, 지인들과 함께 이야기를 나눠보세요"></Title>
+            </div>
+            <div className="login-box">
+                <div>
+                    <Input title={true} type="text" placeholder="이메일 또는 전화번호" onChange={onChangeUserid}/>
+                    <Input title={true} type="password" placeholder="비밀번호" onChange={onChangePassword}/>
+
+                    <Button type={"primary"} className="login-button" onClick={onClickLogin} text={"로그인"}/>
+                    <a onClick={() => window.location.href = "/identify"}>비밀번호를 잊으셨나요?</a>
+                    <Linebar/>
+                    <div className="regist">
+                        <Button type={"secondary"} onClick={() => window.location.href = "/regist"} text="새 계정 만들기"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
